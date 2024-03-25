aa2d5ec7f21c515a93811f5a03ed7e59254797c6
Ranya <124516985+ranyaprax@users.noreply.github.com>
2024-03-25 10:41:44 +0000

    fixed links for navbar

--- personal/timer.js
+++ personal/timer.js
@@ -99,25 +99,24 @@ function playAlert() {
   var audio = document.getElementById("breakTimeAudio");
   audio.play();
 }
-
 function loggedin(){
     if(localStorage.getItem("SwiftUserSignedIn") === 'true'){
         document.getElementById("temp").innerHTML = "Log Out";
-        document.getElementById("temp").href = "landing.html";
+        document.getElementById("temp").href = "/landing/landing.html";
         document.getElementById("temp").name = "logOut";
         document.getElementById("home").innerHTML = localStorage.getItem("username")+" | Swift";
-        document.getElementById("timer").href = "personal.html";
-        document.getElementById("forum").href = "forum.html";
-        document.getElementById("settings").href = "settings.html";
+        document.getElementById("timer").href = "/personal/personal.html";
+        document.getElementById("forum").href = "/forum/forum.html";
+        document.getElementById("settings").href = "/settings/settings.html";
     }
     else {
         document.getElementById("temp").innerHTML = "Sign Up";
-        document.getElementById("temp").href = "signUp.html";
+        document.getElementById("temp").href = "/signUp/signUp.html";
         document.getElementById("temp").name = "signOut";
         document.getElementById("home").innerHTML = "Home | Swift";
-        document.getElementById("timer").href = "signUp.html";
-        document.getElementById("forum").href = "signUp.html";
-        document.getElementById("settings").href = "signUp.html";
+        document.getElementById("timer").href = "/signUp/signUp.html";
+        document.getElementById("forum").href = "/signUp/signUp.html";
+        document.getElementById("settings").href = "/signUp/signUp.html";
     }
 }
 window.onload = loggedin;
