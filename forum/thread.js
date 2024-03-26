708d3616fc4a228fe95545c6d3a4d9f54574a066
Ranya <124516985+ranyaprax@users.noreply.github.com>
2024-03-26 16:30:25 +0000

    Update thread.js

--- forum/thread.js
+++ forum/thread.js
@@ -150,11 +150,12 @@ document.getElementById('deleteThreadButton').addEventListener('click', deleteTh
 document.getElementById('addCommentButton').addEventListener('click', addComment);
 refreshThread();
 
-function loggedin(){
-    if(localStorage.getItem("SwiftUserSignedIn") === 'true'){
+//dynamic navbar
+function loggedin() {
+    if (localStorage.getItem("SwiftUserSignedIn") === 'true') {
         document.getElementById("temp").innerHTML = "Log Out";
-        document.getElementById("temp").href = "/landing/landing.html";
         document.getElementById("temp").name = "logOut";
+        document.getElementById("temp").href = "/landing/landing.html";
         document.title = localStorage.getItem("username") + " | Swift";
         document.getElementById("timer").href = "/personal/personal.html";
         document.getElementById("forum").href = "/forum/forum.html";
@@ -164,10 +165,17 @@ function loggedin(){
         document.getElementById("temp").innerHTML = "Sign Up";
         document.getElementById("temp").href = "/signUp/signUp.html";
         document.getElementById("temp").name = "signOut";
-        document.title = "Threads | Swift";
+        document.title = "Timer/Tasks | Swift";
         document.getElementById("timer").href = "/signUp/signUp.html";
         document.getElementById("forum").href = "/signUp/signUp.html";
         document.getElementById("settings").href = "/signUp/signUp.html";
     }
 }
+function loggedOut() {
+    if (document.getElementById("temp").name === "logOut") {
+        localStorage.setItem("logOut",true);
+        localStorage.setItem("landing",false);
+    }
+}
+
 window.onload = loggedin;
