8d406c5906b41e2dc49dde75d9ce35e945d69d5e
ashlynbuckley <ashlynbuckleyemail3@gmail.com>
2024-03-27 20:22:24 +0000

    validate updates

--- settings/settings.js
+++ settings/settings.js
@@ -124,6 +124,8 @@ function updateProfile() {
 }
 
 function updateAccount() {
+
+    if (validateForm()) {
     fetch("https://updateaccount-pgktbhms6a-uc.a.run.app/", {
         method: 'PUT',
         headers: {
@@ -150,7 +152,56 @@ function updateAccount() {
       .catch(error => {
         console.error('Error:', error);
       });
+    }
 
+}
+
+//checks if the email is valid
+function validateEmail(email){
+    //check if there is an @ sign and "."
+    //check if there are characters infront and behind the @
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    if (emailRegex.test(email)) {
+        return true;
+        // Further actions for a valid email (e.g., submit to a server)
+    } else {
+       // alert('Invalid email. Please enter a valid email address.');
+        alert("Invalid email. Please make sure the following are in your email address: '@'  '.' ");
+        return false;
+    }
+}
+
+function validatePassword(password){
+    
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])(?=.*\d)[A-Za-z\d@$!%*?&]{6,20}$/;
+    if(passwordRegex.test(password)){
+        return true;
+    }
+    else{
+        alert("Invalid Password. Please make sure the following are in your Password: 'A-Z'  'a-z' '@$!%*?&' '1-9' and that its between 6 and 50 characters");
+        return false;
+    }
+}   
+
+function validateForm() {
+    // Regular expression for basic email validation
+    // var emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value
+    // Check email format
+    if (!validateEmail(email)) {
+        console.log("Invalid email");
+        return false;
+    }
+
+    // Check minimum password length
+    if (!validatePassword(password)) {
+        console.log("Invalid password");
+        return false;
+    }
+
+    // If both email and password are valid, submit the form
+    return true;
 }
 
 //dynamic navbar
