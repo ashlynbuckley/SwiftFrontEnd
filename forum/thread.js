42dfdd30c1261b8a43802a4c1581294dfbfa6818
RaifCos <Raifcostellobusiness@gmail.com>
2024-03-28 10:55:30 +0000

    fixed deleteComment

--- forum/thread.js
+++ forum/thread.js
@@ -144,9 +144,11 @@ async function updateComments() {
                 <div class="commentBottom">
                     ${comment.content}
                 </div>
-                <button id="deleteCommentButton" onclick="deleteComment('${comment.id}')"> Delete Comment </button>
+                <button id="deleteCommentButton"> Delete Comment </button>
             `;
       commentList.appendChild(li);
+      // Add event listener to delete button
+      li.querySelector(".deleteCommentButton").addEventListener("click", () => { deleteComment(comment.id); });
     });
 
     // Update Comment Count
